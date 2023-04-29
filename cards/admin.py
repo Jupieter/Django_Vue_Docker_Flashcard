9149497c7d12042a528491702186628ac1161ebf
Jupieter <peterjung777@gmail.com>
2023-04-29 19:14:52 +0200

    Admi show the details

--- cards/admin.py
+++ cards/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Card, CardSets
 
-admin.site.register(Card)
-admin.site.register(CardSets)
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question')
+
+@admin.register(CardSets)
+class CardSetsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'set_name')
