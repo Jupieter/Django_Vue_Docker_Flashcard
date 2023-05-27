95e270d21118580454262875f875c035fcd05048
Jupieter <peterjung777@gmail.com>
2023-05-27 17:21:19 +0200

    New Set POST function added tofrontend

--- frontend/src/components/TasksItem.vue
+++ frontend/src/components/TasksItem.vue
@@ -1,19 +1,21 @@
 <!-- filename: TasksItem.vue -->
 <template>
-    <div class="form">
-        <form v-on:submit.prevent="submitForm">
-            <div class="form-group">
-                <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" v-model="title">
-            </div>
-            <div class="form-group">
-                <label for="description">Description</label>
-                <textarea class="form-control" id="description" v-model="description"></textarea>
-            </div>
-            <div class="form-group">
-                <button type="submit">Add Task</button>
-            </div>
-        </form>
+    <div class="container">
+        <div class="form">
+            <form v-on:submit.prevent="submitForm">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" v-model="title">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" id="description" v-model="description"></textarea>
+                </div>
+                <div class="form-group">
+                    <button class= "button submit_button" type="submit">Add New Set</button>
+                </div>
+            </form>
+        </div>
     </div>
 
     <div class="tasks_container">
@@ -62,6 +64,27 @@
                     console.log(error);
                 }
             },
+
+            submitForm() {
+                // Form adatok elküldése a szerverre
+                const formData = {
+                    set_name: this.title,
+                    set_description: this.description,
+                };
+
+                // AJAX kérés küldése a szerverre
+                console.log(formData);
+                axios
+                    .post('http://127.0.0.1:8000/api/create_set/', formData)
+                    .then(response => {
+                    // Sikeres válasz esetén itt dolgozhatod fel a választ
+                    console.log(response.data);
+                    })
+                    .catch(error => {
+                    // Hiba esetén itt kezelheted a hibát
+                    console.error(error);
+                    });
+            },
         },
         created() {
             // Fetch tasks on page load
@@ -69,3 +92,21 @@
         }
     }
 </script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+.form {
+  width: 33%;
+  max-width: 1024px;
+}
+
+@media (max-width: 1024px) {
+  .form {
+    width: 80%;
+  }
+}
+</style>
