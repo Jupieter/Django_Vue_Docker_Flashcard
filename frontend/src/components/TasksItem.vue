f375b28e34f0cdf0311f9912461af40101e01e1d
Jupieter <peterjung777@gmail.com>
2023-05-30 17:14:29 +0200

    new idea

--- frontend/src/components/TasksItem.vue
+++ frontend/src/components/TasksItem.vue
@@ -1,8 +1,43 @@
 <!-- filename: TasksItem.vue -->
 <template>
-    <div class="container">
-        <div class="form">
-            <form v-on:submit.prevent="submitForm">
+    <div>
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">Flash Card</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Card Sets</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Study</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  
+      <!-- Add Set Modal -->
+      <div class="modal fade" id="addSetModal" tabindex="-1" aria-labelledby="addSetModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="addSetModalLabel">Add New Set</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form v-on:submit.prevent="submitForm">
                 <div class="form-group">
                   <label for="title">Name of New Set</label>
                   <input type="text" class="form-control" id="title" v-model="title">
@@ -14,24 +49,40 @@
                   <button type="submit" class="btn btn-primary">Save</button>
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
-            </form>
+              </form>
+            </div>
+          </div>
         </div>
-    </div>
-
-    <div class="tasks_container">
-        <div class="tasks_content">
-            <h1>Cards</h1>
-            <ul class="tasks_list">
+      </div>
+  
+      <!-- Card Sets -->
+      <div class="container-lg">
+        <div class="content">
+            <h4></h4>
+            <h1>Card Sets 
+            <button class="btn btn-primary" href="#" data-bs-toggle="modal" data-bs-target="#addSetModal">New</button>
+            </h1>
+            <ul class="list-group">
                 <li v-for="task in tasks" :key="task.id" class="card">
-                    <h2>{{ task.id}} - {{ task.set_name }}</h2>
+                <div class="card-header bg-gray border-sdarkgrey">
+                    <div class="row">
+                    <div class="col-1 d-flex justify-content-center">
+                        <input type="checkbox" v-model="selectedTasks" :value="task.id">
+                    </div>
+                    <div class="col-9 d-flex justify-content-start">
+                        <h5 class="card-title">{{ task.id}} - {{ task.set_name }}</h5>
+                    </div>
+                    <div class="col-2 d-flex justify-content-end">
+                        <button class="btn btn-danger" type="button" @click="deleteTask(task)">Del</button>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body">
                     <p>{{ task.set_description}}</p>
-                    <button @click="toggleTask(task)">
-                        {{ task.completed ? 'Undo' : 'Complete' }}
-                    </button>
-                    <p></p>
-                    <!-- <button @click="deleteTask(task)">Delete</button> -->
+                </div>
                 </li>
-            </ul>
+            <br>
+          </ul>
         </div>
       </div>
     </div>
